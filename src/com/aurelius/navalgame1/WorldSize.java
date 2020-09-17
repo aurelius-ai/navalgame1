f15d17bbdf28667a08d1d065e7767cb99ed38a78
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 16:25:23 -0500

    pushed WorldSize and WorldStatus classes to pavo folder

---
+++ src/com/aurelius/navalgame1/WorldSize.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2012 Oliver Aurelius Ellison
+
+ */
+package com.aurelius.navalgame1.pavo;
+
+public enum WorldSize {
+	WORLD_HUGE,
+	WORLD_LARGE,
+	WORLD_MEDIUM,
+	WORLD_SMALL,
+	WORLD_TINY
+}
---
+++ src/com/aurelius/navalgame1/WorldStatus.java
@@ -0,0 +1,46 @@
+package com.aurelius.navalgame1.pavo;
+
+public class WorldStatus {
+	private WorldSize ws;
+	private TimeManager tm;
+	private boolean _generationFlag = false;
+	public WorldStatus(WorldSize ws, TimeManager tm) {
+		this.ws = ws;
+		this.tm = tm;
+	}
+	
+	public void NOTOUCH_930202894(int flag) {
+		if (flag == 0)
+			_generationFlag = true;
+		else
+			_generationFlag = false;
+	}
+	
+	public int getCurrentMinutes() {
+		return tm.getCurrentMinutes();
+	}
+	
+	public int getCurrentHour() {
+		return tm.getCurrentHour();
+	}
+	
+	public int getWorldWidth() {
+		return PavoHelper.getGameWidth(ws);
+	}
+	
+	public int getWorldHeight() {
+		return PavoHelper.getGameHeight(ws);
+	}
+	
+	public int getChunksWidth() {
+		return PavoHelper.getGameWidth(ws) * 2;
+	}
+	
+	public int getChunksHeight() {
+		return PavoHelper.getGameHeight(ws) * 2;
+	}
+	
+	public boolean isDoneGenerating() {
+		return _generationFlag;
+	}
+}
