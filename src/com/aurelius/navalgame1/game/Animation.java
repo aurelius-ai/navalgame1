db149493ef1a37207b1f6c529e3c9222c8dd0e68
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 15:45:57 -0500

    first push of Animation extending Entity

---
+++ src/com/aurelius/navalgame1/game/Animation.java
@@ -0,0 +1,72 @@
+package com.aurelius.navalgame1.game.entity;
+
+import com.aurelius.navalgame1.pavo.grid.Entity;
+import com.aurelius.navalgame1.pavo.grid.EntityManager;
+import com.aurelius.navalgame1.pavo.grid.GridedEntityTileOrientation;
+import com.aurelius.navalgame1.pavo.grid.Location;
+
+public class Animation extends Entity {
+	private static final long serialVersionUID = 1L;
+	private GridedEntityTileOrientation[] animationids;
+
+	/**
+	 * @param em The EnitityManager.
+	 * @param loc The Location of the Entity.
+	 * @param animationFrameIds The animation frame sequences.
+	 */
+	public Animation(EntityManager em, Location loc,byte orientation, GridedEntityTileOrientation... animationFrameIds) {
+		super(em, loc, animationFrameIds[0],orientation);
+		animationids = animationFrameIds;
+		handle = 3;
+	}
+	
+	public void setCurrentFrame(int index) {
+		if (index >= getTotalFrames())
+			throw new java.lang.ArrayIndexOutOfBoundsException("The indicated frame is not in the given range of frames.");
+		setId(animationids[index]);
+	}
+	
+	public int getTotalFrames() {
+		return animationids.length;
+	}
+	
+	int nextIndex = 0;
+	boolean direction = true;
+	boolean alternate = true;
+	
+	public void setAlternatingDirection(boolean t) {
+		alternate = t;
+	}
+	
+	public boolean isAlternatingDirections() {
+		return alternate;
+	}
+	
+	public void updateFrame() {
+		if (isHidden()) {
+			return;
+		}
+		setCurrentFrame(nextIndex);
+		if (alternate) {
+		if (direction)
+			nextIndex++;
+		else
+			nextIndex--;
+		if (nextIndex >= getTotalFrames()) {
+			direction = false;
+			nextIndex--;
+		}
+		if (nextIndex == -1) {
+			direction = true;
+			nextIndex = 0;
+		}
+		}
+		else {
+			if (nextIndex >= getTotalFrames() - 1) {
+				nextIndex = 0;
+			}
+			else
+				nextIndex++;
+		}
+	}
+}
