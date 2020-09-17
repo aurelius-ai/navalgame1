090f622a3717d000f6c328000242c4c4000a476e
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 15:35:18 -0500

    first push of NavalServer to extend PavoServer

---
+++ src/com/aurelius/navalgame1/game/NavalServer.java
@@ -0,0 +1,35 @@
+package com.aurelius.navalgame1.game;
+
+import com.aurelius.navalgame1.pavo.Game;
+import com.aurelius.navalgame1.pavo.io.PavoServer;
+
+public class NavalServer extends PavoServer {
+	Game game;
+	/**
+	 * @param ipaddress
+	 */
+	public NavalServer(Game game) {
+		super();
+		this.game = game;
+	}
+	
+	public void onClientConnect() {
+		game.akamaideli3242very();
+		send("SEED:"+Game.Settings.seed);
+	}
+	
+	public void onMessageRecieved(String message) {
+		if (message.startsWith("bounds:")) {
+			String part = message.replace("bounds:","");
+			String col = part.substring(0, part.indexOf(","));
+			String row = part.substring(part.indexOf(",")+1);
+			
+			int x = Integer.parseInt(col);
+			int y = Integer.parseInt(row);
+			NavalGame gn = (NavalGame)game;
+			gn.getMap().setMultiplayer(x, y);
+		}
+		else
+			super.onMessageRecieved(message);
+	}
+}
