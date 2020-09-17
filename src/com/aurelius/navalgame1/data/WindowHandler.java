52064680835fe8ecd4f3c7b7cfe1962f5158f1a5
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 14:56:41 -0500

    first push of WindowHandler

---
+++ src/com/aurelius/navalgame1/data/WindowHandler.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 Oliver Aurelius Ellison
+
+ */
+package com.aurelius.navalgame1.data;
+
+import com.aurelius.navalgame1.gui.*;
+import com.jpii.gamekit.toaster.Toaster;
+
+public class WindowHandler extends com.jpii.gamekit.gui.WindowHandler{
+	
+	private static Toaster toasterManager;
+	
+	public WindowHandler(int defaultx, int defaulty) {
+		super(defaultx, defaulty);
+		new LoginWindow();
+	
+		new GameOverWindow();
+		toasterManager = new Toaster();
+	}
+	
+	/**
+	 * Returns current instance of <code>Toaster</code>. Used to send desktop notifications.
+	 * @return toasterManager
+	 */
+	public Toaster getToasterManager() {
+		return toasterManager;
+	}
+}
