690a954350adbeded37ffba86959a4393dbb2468
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 19:09:12 -0500

    first push of PMouseEvent and PMouseListener

---
+++ src/com/aurelius/navalgame1/pavo/gui/PMouseEvent.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.aurelius.navalgame1.pavo.gui.events;
+
+/**
+ * @author MKirkby
+ *
+ */
+public abstract class PMouseEvent implements PMouseListener {
+	public void mouseDown(int x, int y, int buttonid) {
+	}
+	public void mouseUp(int x, int y, int buttonid) {
+	}
+	public void mouseHover(int x, int y) {
+	}
+	public void mouseDrag(int x, int y) {
+	}
+}
---
+++ src/com/aurelius/navalgame1/pavo/gui/PMouseListener.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.aurelius.navalgame1.pavo.gui.events;
+
+/**
+ * @author MKirkby
+ *
+ */
+public interface PMouseListener {
+	public void	mouseDown(int x, int y, int buttonid);
+	public void	mouseUp(int x, int y, int buttonid);
+	public void mouseHover(int x, int y);
+	public void mouseDrag(int x, int y);
+}
