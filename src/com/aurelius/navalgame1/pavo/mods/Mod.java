3c2dd10fe34bbd3bfd6373516b5bca4b12e642d8
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 21:08:11 -0500

    pushed new mods for pavo

---
+++ src/com/aurelius/navalgame1/pavo/mods/Mod.java
@@ -0,0 +1,49 @@
+package com.aurelius.navalgame1.pavo.mods;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+import com.aurelius.navalgame1.pavo.Chunk;
+import com.aurelius.navalgame1.pavo.Game;
+import com.aurelius.navalgame1.pavo.World;
+
+public class Mod {
+	public Mod() {
+		
+	}
+	public void onGameLoad(Game g) {
+		
+	}
+	public void onMouseMove(MouseEvent me) {
+		
+	}
+	public void onKeyDown(KeyEvent ke) {
+		
+	}
+	public void onMouseDown(MouseEvent me) {
+		
+	}
+	public void onMouseUp(MouseEvent me) {
+		
+	}
+	public void onGameClose(Game g) {
+		
+	}
+	public void onGameSave(Game g,File path) {
+		
+	}
+	public void onGameLoad(Game g,File path) {
+		
+	}
+	public void onChunkRender(Chunk c, Graphics2D g) {
+		
+	}
+	public void onWorldRender(World w,Graphics2D g) {
+		
+	}
+	public void onWorldTick(World w) {
+		
+	}
+}
