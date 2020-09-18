c282024b8d4e230e7b3b6309ca16fa58f1ee7b81
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2020-09-17 21:04:19 -0500

    first push of renderer dir and files

---
+++ src/com/aurelius/navalgame1/renderer/RenderConstants.java
@@ -0,0 +1,12 @@
+package com.aurelius.navalgame1.renderer;
+
+import java.awt.Color;
+/**
+ * A bunch of rendering constants that effect stuff in com.jpii.navalbattle.renderer.
+ */
+public class RenderConstants {
+    public static Color GEN_SAND_COLOR = new Color(77, 95, 44);
+    public static int NIGHT_MAX_DARKNESS = 100;
+    public static double CURRENT_TIME_OF_DAY = 60;
+    public static int DAYNIGHT_LENGTH_IN_SECONDS = 120;
+}
